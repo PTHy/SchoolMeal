e69afb2796fb2b350c41859f9b567e5575f76fae
PTHy <30455428+PTHy@users.noreply.github.com>
2018-07-24 19:40:58 +0900

    [ADD] GET Meal auto
    시간 차이 계산 공부해야된다

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const request = require('request')
 const cheerio = require('cheerio')
+const moment = require('moment')
 
-router.get('/', async (req,res) => {
+router.get('/:time', async (req,res) => {
+  const now = moment(new Date(req.params.time)).format("YYYY.MM.DD")
   let data = [];
-  let url = "http://stu.dge.go.kr/sts_sci_md01_001.do?schulCode=D100000282&schulCrseScCode=4&schMmealScCode=&schYmd=2018.07.12"
+  let url = "http://stu.dge.go.kr/sts_sci_md01_001.do?schulCode=D100000282&schulCrseScCode=4&schMmealScCode=&schYmd="+now
   let dates = [];
   let meals = [];
 
@@ -31,6 +33,14 @@ router.get('/', async (req,res) => {
   await selectData
 
   const insertData = new Promise((resolve,reject) => {
+
+    if(meals.length === 0){
+      return res.send({
+              "result" : 0,
+              "error" : "급식이 없는 날이에요 ㅜㅜ"
+            })
+    }
+
     for (var i = 0; i < dates.length; i++) {
       data.push({date : dates[i], meal : meals[i]});
     }
@@ -39,8 +49,65 @@ router.get('/', async (req,res) => {
 
   await insertData
 
-  res.send(data)
+  res.send({
+    "result" : 1,
+    "data" : data
+  })
 
+})
+
+router.get('/',async (req,res) => {
+  let data = [];
+  let url = "http://stu.dge.go.kr/sts_sci_md01_001.do?schulCode=D100000282&schulCrseScCode=4&schMmealScCode=&schYmd=2017.12.21"
+  let dates = [];
+  let meals = [];
+  const now = moment().format("YYYY.MM.DD")
+  const time = moment().format("HH:mm")
+
+  const selectData = new Promise((resolve,reject) => {
+      request(url,async (error,response,html) => {
+        if(error) reject(error)
+
+        let $ = cheerio.load(html)
+
+        $('table > thead > tr > th').slice(1).each(await function(){
+          let date_temp = $(this);
+          dates.push(date_temp.text())
+        })
+
+        $('table > tbody > tr:nth-child(2) > td').each(await function(){
+          let meal_temp = $(this);
+          meals.push(meal_temp.text());
+        })
+
+        resolve();
+      });
+    })
+
+  await selectData
+
+  const insertData = new Promise((resolve,reject) => {
+
+    if(meals.length === 0){
+      return res.send({
+              "result" : 0,
+              "error" : "급식이 없는 날이에요 ㅜㅜ"
+            })
+    }
+
+    for (var i = 0; i < dates.length; i++) {
+      data.push({date : dates[i], meal : meals[i]});
+    }
+    resolve()
+  })
+
+  await insertData
+
+
+  res.send({
+    "result" : 1,
+    "data" : data
+  })
 })
 
 module.exports = router
